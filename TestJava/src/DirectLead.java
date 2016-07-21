bf0d34af51bba658e2edc5291baf757dede34676
vsanoglu <volkansng@gmail.com>
2016-07-21 11:32:24 +0300

    Classes Added For Chrome

--- TestJava/src/DirectLead.java
+++ TestJava/src/DirectLead.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -7,7 +6,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.ie.*;
-import org.openqa.selenium.security.UserAndPassword;
 import org.openqa.selenium.support.ui.*;
 
 //import components.ComboBox;
@@ -15,13 +13,20 @@ import org.openqa.selenium.support.ui.*;
 
 public class DirectLead {
 	
+
+
 	
 	public static void main(String[] args) {
 try {
+	
 	//Comment out unused browsers
+
+	Chrome(ChromeLogin());
+	
+	
 	//LoginIE();
-	//Chrome();
-	LoginFirefox();
+	
+	//LoginFirefox();
 	
 	
 } 
@@ -39,14 +44,21 @@ public static void LoginFirefox(){
 	
 File pathToBinary = new File("C:\\Users\\vsanoglu\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
 FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
-FirefoxProfile firefoxProfile = new FirefoxProfile();       
+FirefoxProfile firefoxProfile = new FirefoxProfile(); 
 WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
-//driver.get("https://crmdevinternal.bupa.com.sa/BupaSIT/main.aspx?etn=lead&id=&pagetype=entityrecord");
-driver.get("https://admin:admin@crmdevinternal.bupa.com.sa/BupaSIT/main.aspx?etn=lead&id=&pagetype=entityrecord");
+driver.get("https://bupacrm2.veripark.com:444/main.aspx?etn=lead&id=&pagetype=entityrecord");
+WebElement UserElement = driver.findElement(By.xpath(".//*[@id='userNameInput']"));
+UserElement.sendKeys("crm12\\crmadmin");
+WebElement PassElement = driver.findElement(By.xpath(".//*[@id='passwordInput']"));
+PassElement.sendKeys("Tt12345");
+WebElement SubmitElement = driver.findElement(By.xpath(".//*[@id='submitButton']"));
+SubmitElement.click();
+
+
+
 
 
 }
-
 
 public static void LoginIE(){
 
@@ -74,16 +86,57 @@ public static void LoginIE(){
 		
 }
 
+public static WebDriver ChromeLogin(){ 
 
-public static void Chrome(){
-	
 	String pathToBinary = "C:\\Program Files (x86)\\selenium-2.53.1\\chromedriver.exe";
 	System.setProperty("webdriver.chrome.driver",pathToBinary);
 	WebDriver driver = new ChromeDriver();
-	driver.get("https://crmdevinternal.bupa.com.sa/BupaSIT/main.aspx?etn=lead&id=&pagetype=entityrecord");
-	
+	//WebDriver driver = new ChromeDriver();
+	//driver.get("https://crmdevinternal.bupa.com.sa/BupaSIT/main.aspx?etn=lead&id=&pagetype=entityrecord");
+	driver.get("https://bupacrm2.veripark.com:444/main.aspx?etn=lead&id=&pagetype=entityrecord");
+	WebElement UserElement = driver.findElement(By.xpath(".//*[@id='userNameInput']"));
+	UserElement.sendKeys("crm12\\crmadmin");
+	WebElement PassElement = driver.findElement(By.xpath(".//*[@id='passwordInput']"));
+	PassElement.sendKeys("Tt12345");
+	WebElement SubmitElement = driver.findElement(By.xpath(".//*[@id='submitButton']"));
+	SubmitElement.click();
+	return driver;
 	
 }
+
+public static void Chrome(WebDriver driver){
+	 
+driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	waitForLoad(driver);
+WebElement CheckElement = driver.findElement(By.id("contentIFrame0"));
+driver.switchTo().frame(CheckElement);
+WebElement Newframe = driver.findElement(By.id("campaignid_d"));
+System.out.println(Newframe.getTagName());
+	
+	
+	
+	/*
+	 System.out.println("i am here1");
+	List<WebElement> resultList = driver.findElements(By.id("bupa_lead_expectedaveragepremium"));
+	for (WebElement resultItem : resultList){
+        String tabname=resultItem.getTagName();
+        System.out.println("i am here2");
+        System.out.println(tabname);}
+        
+        */
+}
+
+
+
+
+
+private static void waitForLoad(WebDriver driver) {
+	// TODO Auto-generated method stub
+	new WebDriverWait(driver, 30).until((ExpectedCondition<Boolean>) wd ->
+    ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
+}
+
+
 	
 public static int IeloginWithBat(){
 
@@ -104,7 +157,6 @@ public static int IeloginWithBat(){
 	                 }
 	return 0;
 	               }	
-
 
 
 						}
